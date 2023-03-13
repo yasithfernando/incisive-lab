7bc387a7477e51036a29e21579c5a3b487cfd36d
yasithfernando <iyfernando42@gmail.com>
2023-03-13 21:30:56 +0530

    General Layout FXML Added

--- src/main/java/com/example/incisivelab/GeneralLayoutController.java
+++ src/main/java/com/example/incisivelab/GeneralLayoutController.java
@@ -2,17 +2,12 @@ package com.example.incisivelab;
 
 import javafx.event.ActionEvent;
 
-public class LinearityPageController {
+public class GeneralLayoutController {
 
     public void onResetButtonClick(ActionEvent actionEvent) {
-
     }
 
     public void onNextButtonClick(ActionEvent actionEvent) {
-
-    }
-
-    public void onBackButtonClick(ActionEvent actionEvent) {
 
     }
 }
