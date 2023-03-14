beef29ec123cca92c23655fd54d4cac5d90d0291
yasithfernando <iyfernando42@gmail.com>
2023-03-15 00:43:27 +0530

    Gel Run Working Perfectly

--- src/main/java/com/example/incisivelab/FinalScreenController.java
+++ src/main/java/com/example/incisivelab/FinalScreenController.java
@@ -7,10 +7,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+//import org.apache.pdfbox.pdmodel.PDDocument;
+//import org.apache.pdfbox.pdmodel.PDPage;
+//import org.apache.pdfbox.pdmodel.PDPageContentStream;
+//import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 public class FinalScreenController {
     public void onResetButtonClick(ActionEvent actionEvent) {
@@ -26,22 +26,22 @@ public class FinalScreenController {
     }
 
     public void pdfGeneration(){
-        try {
-            PDDocument document = new PDDocument();
-            PDPage page = new PDPage();
-            document.addPage(page);
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            contentStream.beginText();
-            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            contentStream.newLineAtOffset(100, 700);
-            contentStream.showText("Hello, world!");
-            contentStream.endText();
-            contentStream.close();
-            document.save("output.pdf");
-            document.close();
-            System.out.println("PDF generated successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            PDDocument document = new PDDocument();
+//            PDPage page = new PDPage();
+//            document.addPage(page);
+//            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+//            contentStream.beginText();
+//            contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+//            contentStream.newLineAtOffset(100, 700);
+//            contentStream.showText("Hello, world!");
+//            contentStream.endText();
+//            contentStream.close();
+//            document.save("output.pdf");
+//            document.close();
+//            System.out.println("PDF generated successfully.");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
